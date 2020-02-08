63d66cc6b96d3ea77d43e23924d8df88d34a5242
YASUYUKI <ygt1qa3@gmail.com>
2020-02-09 02:00:31 +0900

    change farmework vue.js to react.js

--- plugins/filters.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-import dayjs from "dayjs";
-
-// 本文のトリミング
-Vue.filter("description", function(value) {
-    if (value) {
-      return value.slice(0, 100) + "...";
-    }
-  })
--- store/state.js
+++
@@ -1,6 +0,0 @@
-export default {
-    // APIのレスポンスデータ
-    lists: [],
-    // ローディング中かどうかを判定する
-    isLoading: false
-}
