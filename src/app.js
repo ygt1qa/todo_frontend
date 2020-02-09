4347b64388ff8131bf7e5c66582c81d5689b31cc
YASUYUKI <ygt1qa3@gmail.com>
2020-02-09 19:49:18 +0900

    work react by webpack

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 class Header extends React.Component {
   render() {
     return <p>This is from Header</p>;
@@ -8,7 +11,8 @@ const jsx = (
   <div>
     <h1>Title</h1>
     <Header />
+    <Header />
+    <Header />
   </div>
 )
-
 ReactDOM.render(jsx, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
+
